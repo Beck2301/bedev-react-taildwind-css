7ee37b202692039c61f5064f40deaaf85dc991db
Bryan Escobar <50363542+Beck2301@users.noreply.github.com>
2021-08-21 22:07:11 -0600

    changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,7 +16,21 @@ function AboutMe() {
                         <p className="md:w-11/12 md:text-2xl  mb-5 ">My nameis <b className="text-yellow-500">Bryan Escobar</b>, i am a person who loves challenges and does not give up easily. Very detailed, I never stop learning, easy to socialize and deal with people, web developer and lover of the frontend world.</p>
 
                         <h1 class="text-5xl  md:text-6xl mb-5 font-bold" >Academic training </h1>
-                      
+
+                        <div class="container container  flex flex-wrap flex-col md:flex-row">
+            
+                            <div class="w-full lg:w-1/2 p-3">
+                                <div class="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-28 md:w-72  shadow shadow-lg  ">
+                                    <img class="block h-auto w-full lg:w-auto  flex-none bg-cover bg-white md:p-2 p-10  " src="https://i.ibb.co/fF2ccz8/logo-udb-transparente.png" />
+                                    <div class="bg-yellow-500 rounded-b lg:rounded-b-none lg:rounded-r  p-4 flex flex-col justify-between leading-normal">
+                                        <div class="text-black font-bold text-xl mb-2 leading-tight">Computer Engineering Technician</div>
+                                      
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+
 
                     </div>
                 </div>
